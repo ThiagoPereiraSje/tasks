831ab0d594d4008fa9d61e4e07acdd6d9c9f9068
Thiago Pereira <thiagopereiramail@gmail.com>
2019-11-28 08:44:38 -0300

    Configurando o carregamento das fontes.

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import * as Font from 'expo-font';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export class App extends Component {
+  async componentDidMount() {
+    await Font.loadAsync({
+      'Lato': require('./assets/fonts/Lato.ttf'),
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello World!</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +25,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  text: {
+    fontFamily: 'Lato',
+  },
+})
+
+export default App;
